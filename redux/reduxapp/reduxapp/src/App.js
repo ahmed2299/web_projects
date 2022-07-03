f00d8c4f5c6e81a1f511ee5fac0ed336d7d7ba24
ahmed2299 <55079510+ahmed2299@users.noreply.github.com>
2022-07-03 21:19:24 +0200

    Dev:use state

---
+++ redux/reduxapp/reduxapp/src/App.js
@@ -0,0 +1,33 @@
+import React,{Component} from "react";
+
+
+class App extends Component{
+
+    state={
+        count:0
+    }
+
+    increase=()=>{
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+
+    decrease=()=>{
+        this.setState({
+            count:this.state.count-1
+        })
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <button onClick={this.increase}> + </button>
+                <div> {this.state.count} </div>
+                <button onClick={this.decrease}> - </button>      
+            </div>       
+            )
+    }
+}
+
+export default App;
